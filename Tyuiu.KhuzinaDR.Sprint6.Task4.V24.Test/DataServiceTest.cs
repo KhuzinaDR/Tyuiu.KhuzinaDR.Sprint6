d47a0e2f6760283cf470c5a4646f71bfedc4b347
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2025-01-24 19:14:44 +0300

    Task4

---
+++ Tyuiu.KhuzinaDR.Sprint6.Task4.V24.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.KhuzinaDR.Sprint6.Task4.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
