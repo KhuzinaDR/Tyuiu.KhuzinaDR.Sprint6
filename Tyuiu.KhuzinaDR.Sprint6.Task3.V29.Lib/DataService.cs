907e6180bff0316d7a752e54bc88af3a3629d99e
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2025-01-24 19:10:56 +0300

    Task3

---
+++ Tyuiu.KhuzinaDR.Sprint6.Task3.V29.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.KhuzinaDR.Sprint6.Task3.V29.Lib
+{
+    public class DataService : ISprint6Task3V29
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+            int[] newcol = new int[rows];
+            for (int i = 0; i < rows; i++)
+            {
+                newcol[i] = matrix[i, columns - 1];
+            }
+            Array.Sort(newcol);
+            for (int i = 0; i < rows; i++)
+            {
+                matrix[i, columns - 1] = newcol[i];
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.KhuzinaDR.Sprint6.Task3.V29/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.KhuzinaDR.Sprint6.Task3.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
