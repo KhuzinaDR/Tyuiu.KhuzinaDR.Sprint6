b318db7fd33a049c2cd3f0ff2c387285a69410a9
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2025-01-24 19:22:18 +0300

    Task6

---
+++ Tyuiu.KhuzinaDR.Sprint6.Task6.V12/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.KhuzinaDR.Sprint6.Task6.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
