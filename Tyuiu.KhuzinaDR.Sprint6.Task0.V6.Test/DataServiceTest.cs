6600278f73fea5a24304f2f9b74dd319b008fffb
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2025-01-24 18:56:46 +0300

    Add project files.

---
+++ Tyuiu.KhuzinaDR.Sprint6.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.KhuzinaDR.Sprint6.Task0.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
